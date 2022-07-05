653661b5072b3883174f89521f90384a7ef87e8b
Eugeny Kuzakov <kuzakov@satel.org>
2022-07-05 20:43:37 +0300

    Added pretty table support

--- kube-limits-cals.sh
+++ kube-limits-cals.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+pt=prettytable.sh/prettytable.sh
+chmod +x "${pt}"
 for ns in $(kubectl get ns --no-headers=true | awk '{print $1}'); do
 	for type in deploy ds statefulsets; do
 		for deploy in $(kubectl -n "${ns}" get "${type}" --no-headers=true 2>/dev/null | awk '{print $1}' ) ; do
 		    tmp="$(mktemp)"
 			kubectl -n "${ns}" get "${type}" "${deploy}" -o json > "${tmp}"
-			replicas=$(cat "${tmp}" | jq '" \(.spec.replicas) "' | sed 's/"//g' | sed -r 's/\ //g')
-			for container in $(cat "${tmp}" | jq '" \(.spec.template.spec.containers[].name)"' | sed 's/"//g' | sed -r 's/\ //g'); do
+			replicas=$(cat "${tmp}" | jq '" \(.spec.replicas) "' | sed -r 's/("|\ )//g')
+			for container in $(cat "${tmp}" | jq '" \(.spec.template.spec.containers[].name)"' | sed -r 's/("|\ )//g'); do
 				printf "${ns} ${type} ${deploy} ${replicas} ${container}"
 				cat "${tmp}" | jq '.spec.template.spec.containers[] | select (.name=="'${container}'")  | " \(.resources.limits.cpu) \(.resources.limits.memory)"' | sed 's/"//g'
 				
@@ -60,5 +62,5 @@ done |
 			}			
 		}
 		{ 
-			print $1,$2,$3,$4,$5,calc_cpu($6),calc_ram($7)
-		}'
+			printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1,$2,$3,$4,$5,calc_cpu($6),calc_ram($7))
+		}' | "${pt}"
