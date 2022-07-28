541aa9a9b22ce77c53270db04ecb5ccc85392b28
Eugeny Kuzakov <kuzakov@satel.org>
2022-07-28 21:52:08 +0300

    1. added prettytable.sh download&use cached. 2. using md5sum to check downloaded file

--- kube-limits-cals.sh
+++ kube-limits-cals.sh
@@ -2,7 +2,7 @@
 
 usage()
 {
-	cat << END
+	cat << END 1>&2
 ${0} options:
 
 	--tabbed -- switch pretty output to space delimited (to use in spreadsheets&etc)
@@ -10,27 +10,47 @@ END
 	exit 1
 }
 
+log(){
+	echo "--- ${*}" 1>&2
+}
+
 jq="$(which jq)"
 if [ ! -x "${jq}" ]; then
-	echo Need jq
+	log Need jq
 	exit 1
 fi
 
 bash="$(which bash)"
 if [ ! -x "${bash}" ]; then
-	echo Need bash
+	log Need bash
 	exit 1
 fi
 
 tail="$(which tail)"
 if [ ! -x "${tail}" ]; then
-	echo Need tail
+	log Need tail
 	exit 1
 fi
 
 sed="$(which sed)"
 if [ ! -x "${sed}" ]; then
-	echo Need sed
+	log Need sed
+	exit 1
+fi
+
+wget="$(which wget)"
+if [ ! -x "${sed}" ]; then
+	log wget not found. It may be required
+fi
+
+md5sum="$(which md5sum)"
+if [ ! -x "${md5sum}" ]; then
+	log md5sum not found. It may be required
+fi
+
+kubectl="$(which kubectl)"
+if [ ! -x "${kubectl}" ]; then
+	log Need kubectl
 	exit 1
 fi
 
@@ -38,7 +58,7 @@ pretty=1
 
 if [ $# -gt 0 ]; then
 	if [ "${1}" = "--tabbed" ]; then
-		echo Pretty out is off 1>&2
+		log Pretty out is off 
 		pretty=0
 		shift
 	fi
@@ -49,18 +69,63 @@ if [ $# -gt 0 ]; then
 		usage
 	fi	
 	if [ -n "${1}" ]; then
-		echo Unknown flag
+		log Unknown flag
 		usage
 	fi
 fi
 
-pt=prettytable.sh/prettytable.sh
+pt_url="https://github.com/jakobwesthoff/prettytable.sh/raw/c5d52169e9bf6ab6a56595f8a9084e9bcd30bc5a/prettytable.sh"
+pt_md5="404f68d34943f8ca20aa326d0c79ca23"
 
-for ns in $(kubectl get ns --no-headers=true | awk '{print $1}'); do
+md5_check() {
+	if [ $# -ne 2 ]; then
+		log Usage: md5_check "${pt_md5} pretytable.sh" 
+		exit 1
+	fi
+	
+	echo "${1} ${2}" | "${md5sum}" -c 1>&2
+	return $?
+}
+
+download_pretytable() {
+	local pt="${1}"
+	log "Downloading ${pt_url} -> ${pt}"
+	wget_log="$(mktemp)"
+	log "$("${wget}" -c -t 3 -O "${pt}" "${pt_url}" 2>&1 | tee "${wget_log}" | grep saved )"
+	
+	if md5_check "${pt_md5}" "${pt}"; then
+		log  "MD5 sum checked"
+	else
+		log "Unable to download prettytable.sh from ${pt_url} and save to ${pt}"
+		cat "${wget_log}" 1>&2
+		exit 1
+	fi
+	rm "${wget_log}"
+}
+
+pt="$(dirname "${0}")/prettytable.sh/prettytable.sh"
+
+if [ ! -r "${pt}" ]; then
+	pt="${0}.prettytable.sh"
+	if [ ! -r "${pt}" ]; then
+		download_pretytable "${pt}"
+	else
+		
+		if md5_check "${pt_md5}" "${pt}"; then
+			log Using cached "${pt}"
+		else
+			download_pretytable "${pt}"
+		fi
+	fi
+fi
+
+
+
+for ns in $("${kubectl}" get ns --no-headers=true | awk '{print $1}'); do
 	for type in deploy ds statefulsets; do
-		for deploy in $(kubectl -n "${ns}" get "${type}" --no-headers=true 2>/dev/null | awk '{print $1}' ) ; do
+		for deploy in $("${kubectl}" -n "${ns}" get "${type}" --no-headers=true 2>/dev/null | awk '{print $1}' ) ; do
 			tmp="$(mktemp)"
-			kubectl -n "${ns}" get "${type}" "${deploy}" -o json > "${tmp}"
+			"${kubectl}" -n "${ns}" get "${type}" "${deploy}" -o json > "${tmp}"
 			replicas=$("${jq}" '" \(.spec.replicas) "' < "${tmp}"| "${sed}" -r 's/("|\ )//g')
 			for container in $("${jq}" '" \(.spec.template.spec.containers[].name)"' < "${tmp}" | "${sed}" -r 's/("|\ )//g'); do
                     		printf "${ns} ${type} ${deploy} ${replicas} ${container}"
@@ -120,7 +185,15 @@ done |
 		}' | 
 			(
 				if [ "${pretty}" -eq 1 ]; then
-					"${bash}" "${pt}"
+					
+					if "${bash}" "${pt}"; then
+						log some problems with downloaded prettytable.sh. Deleted. Redownloading... 1>&2
+						rm "${pt}"
+						download_pretytable "${pt}"
+						log Restarting "${0}"
+						sleep 1
+						exec "${0}"
+					fi
 				else
 					cat | "${tail}" -n +2
 				fi
